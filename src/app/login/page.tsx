69f84f1efa332730b29e2f218a6679dfc3db1374
Laxmi Lal <112006645+n07Laksh@users.noreply.github.com>
2024-03-01 16:07:17 +0530

    Update page.tsx loading url

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -87,7 +87,7 @@ const Login = () => {
         };
         // Send a POST request
         const response = await axios.post(
-          "https://chat-app-auth.vercel.app/chatapp/user/auth/login",
+          "https://chatappauth.vercel.app/chatapp/user/auth/login",
           // "http://localhost:8000/chatapp/user/auth/login",
           user,
           {
